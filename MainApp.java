2755c33f22323a805eac9420368c5320695077f3
KMZ7878 <marqueskaue771@gmail.com>
2024-06-23 15:35:07 -0300

    ebac exeptions

---
+++ MainApp.java
@@ -0,0 +1,10 @@
+package tabeeel;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Tabela.java
@@ -0,0 +1,5 @@
+package tabeeel;
+
+public class Tabela {
+
+}
